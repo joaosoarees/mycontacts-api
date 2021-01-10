4424ff84dab24a0c8f05d4e669e98998971950a8
João Vitor <jsoareez@gmail.com>
2021-01-10 15:22:23 -0300

    refactor contacts repository delete method to work with postgres and remove mocked data

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'João',
-    email: 'jsoareez@gmail.com',
-    phone: '16992883774',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'José',
-    email: 'jose@gmail.com',
-    phone: '16992883772',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -64,11 +45,10 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+
+    return deleteOp;
   }
 }
 
