d4bd71e25adb4aaa09e2c7822f1e1ae862cae6fe
João Vitor <jsoareez@gmail.com>
2021-01-10 13:06:45 -0300

    create show and delete method

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,18 +1,38 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'João',
     email: 'jsoareez@gmail.com',
     phone: '16992883774',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'José',
+    email: 'jose@gmail.com',
+    phone: '16992883772',
+    category_id: v4(),
   },
 ];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
